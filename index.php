e441fc9c68389091d89ea0e7d1a94c812654857a
hyukawa <hyukawa@live.jp>
2018-03-11 17:53:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
 <title>高田馬場周辺の列車運行情報</title>
 <meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" />
 <!--ビューポート設定-->
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-</head>
+//<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<head>
 <style type="text/css">
 body {background-color: #fff; color: #222; font-family: sans-serif;}
 pre {margin: 0; font-family: monospace;}
 a:link {color: #009; text-decoration: none; background-color: #fff;}
 a:hover {text-decoration: underline;}
-table {border-collapse: collapse; border: 0; width: 100%; cellspacing: 30%; cellpadding: 30%; width: 934px; box-shadow: 1px 2px 3px #ccc;}
-.center {text-align: center;}
-.center table {margin: 1em auto; text-align: left;}
-.center th {text-align: center !important;}
-td, th {border: 0px solid #666; font-size: 125%; vertical-align: baseline; padding: 4px 5px;}
+//table {border-collapse: collapse; border: 0; width: 100%; cellspacing: 30%; cellpadding: 30%; width: 934px; box-shadow: 1px 2px 3px #ccc;}
+//.center {text-align: center;}
+//.center table {margin: 1em auto; text-align: left;}
+//.center th {text-align: center !important;}
+//td, th {border: 0px solid #666; font-size: 125%; vertical-align: baseline; padding: 4px 5px;}
 h1 {font-size: 150%;}
 h2 {font-size: 125%;}
 .p {text-align: left;}
@@ -28,7 +28,6 @@ h2 {font-size: 125%;}
 img {float: right; border: 0;}
 hr {width: 934px; background-color: #ccc; border: 0; height: 1px;}
 </style>
-<title>phpinfo()</title><meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" /></head>
 <body><div class="center">
 
 
