8ae35c70d6320a2734cd9a5b03ce767c153d9951
hyukawa <hyukawa@live.jp>
2018-03-11 15:47:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $railway_html = file_get_contents($url);
 $railway_info = explode("\n", $railway_html);
 
 // 取得結果を配列へ格納
+$led_pat = [];
 $return0_pat = [];
 $return1_pat = [];
 
@@ -38,9 +39,9 @@ for($i=0; $i<count($railway_info)-1; $i++) {
             $pos = strpos($railway_info[$i+1], "遅延");
             if($pos !== false) {
                 // 遅延であればLED色は黄色で点滅
-                //$led_pat += [ "rail_state" => "▲" ];
-                $return0_pat += [ "rail_state" => $railway_info[$i+1] ]];
-                $return0_pat += [ "rail_state_detail" => $railway_info[$i+2] ]];
+                $led_pat += [ "rail_state" => "▲" ];
+                //$return0_pat += [ "rail_state" => $railway_info[$i+1] ]];
+                //$return0_pat += [ "rail_state_detail" => $railway_info[$i+2] ]];
                  break;
             } else {
                 // 平常運転、遅延でもなければLEDを赤点滅して警告する
@@ -55,8 +56,8 @@ for($i=0; $i<count($railway_info)-1; $i++) {
 }
 
 echo "路線： " . $line_name[0] . "</br>";
-echo "運行状況： " . $return0_pat["rail_state"] . "</br>";
-echo "遅延情報： " . $return0_pat["rail_state_detail"] . "</br>";
+//echo "運行状況： " . $return0_pat["rail_state"] . "</br>";
+//echo "遅延情報： " . $return0_pat["rail_state_detail"] . "</br>";
   //遅延情報は文字が途中で切れてしまっているため、サイネージで表示した場合、恰好悪いので表示させない
 
 //var_dump($led_pat["rail_pat"]);
