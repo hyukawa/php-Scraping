5fce3f57fab84b4b7fa08c4a5ccaf26898313213
hyukawa <hyukawa@live.jp>
2018-03-11 14:38:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -29,26 +29,27 @@ for($i=0; $i<count($railway_info)-1; $i++) {
         $pos = strpos($railway_info[$i+1], "平常");
         if($pos !== false) {
             // 平常運転であればLED色は緑で点灯
-            $led_pat += [ "rail_color" => "green" ];
-            $led_pat += [ "rail_pat" => ["on", "on", "on"] ];
+            $led_pat += [ "rail_state" => "○" ];
+            $led_pat += [ "rail_state_detail" => $railway_info[$i+2] ];
         } else {
             // 平常運転でなければ「遅延」が含まれるかチェック
             $pos = strpos($railway_info[$i+1], "遅延");
             if($pos !== false) {
                 // 遅延であればLED色は黄色で点滅
-                $led_pat += [ "rail_color" => "yellow" ];
-                $led_pat += [ "rail_pat" => ["blink", "blink", "blink"] ];
+                $led_pat += [ "rail_state" => "▲" ];
+                $led_pat += [ "rail_state_detail" => ["blink", "blink", "blink"] ];
             } else {
                 // 平常運転、遅延でもなければLEDを赤点滅して警告する
-                $led_pat += [ "rail_color" => "red" ];
-                $led_pat += [ "rail_pat" => ["blink", "blink", "blink"] ];
+                $led_pat += [ "rail_state" => "×" ];
+                $led_pat += [ "rail_state_detail" => ["blink", "blink", "blink"] ];
             }
         }
     }
 }
 
 echo "路線： " . $line_name . "</br>";
-echo "運行状況： " . $led_pat["rail_color"] . "</br>";
+echo "運行状況： " . $led_pat["rail_state] . "</br>";
+echo "遅延情報： " . $led_pat["rail_state_detail] . "</br>";
 //echo "LED点滅パターン:</br>";
 //var_dump($led_pat["rail_pat"]);
 //var_dump( phpinfo() );
