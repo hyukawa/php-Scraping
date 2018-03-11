13a7e9a80c6c01cfbe411a38ec1d1cab7344721a
hyukawa <hyukawa@live.jp>
2018-03-11 18:01:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ a:link {color: #009; text-decoration: none; background-color: #fff;}
 a:hover {text-decoration: underline;}
 //table {border-collapse: collapse; border: 0; width: 100%; cellspacing: 30%; cellpadding: 30%; width: 934px; box-shadow: 1px 2px 3px #ccc;}
  table {width:100%; table-layout: fixed;}
-.center {text-align: center;}
+//.center {text-align: center;}
 //.center table {margin: 1em auto; text-align: left;}
 //.center th {text-align: center !important;}
 //td, th {border: 0px solid #666; font-size: 125%; vertical-align: baseline; padding: 4px 5px;}
@@ -112,9 +112,9 @@ for($i=0; $i<count($railway_info)-1; $i++) {
 print<<<eof
   <table>
    <tr><td bgcolor="#00ff00" colspan="3"><font size="5" color="#ffffff">運行情報</font></td>
-   <tr><td bgcolor="#80c241" width="20%"></td><td width="50%">$line_name[0]</td><td class="center" width="30%">$led_pat[0]</td></tr>
-   <tr><td bgcolor="#009bbf"></td><td>$line_name[1]</td><td class="center">$led_pat[1]</td></tr>
-   <tr><td bgcolor="#9c5e31"></td><td>$line_name[2]</td><td class="center">$led_pat[2]</td></tr>
+   <tr><td bgcolor="#80c241" width="20%"></td><td width="50%">$line_name[0]</td><td width="3%" align="center">$led_pat[0]</td></tr>
+   <tr><td bgcolor="#009bbf"></td><td>$line_name[1]</td><td align="center">$led_pat[1]</td></tr>
+   <tr><td bgcolor="#9c5e31"></td><td>$line_name[2]</td><td align="center">$led_pat[2]</td></tr>
   <table>
 eof;
 
