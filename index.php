3a5717145d716763d931dd846b0f08f79fd9281e
hyukawa <hyukawa@live.jp>
2018-03-29 15:00:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html lang="ja">
 <head>
+<meta http-equiv="refresh" content="60" >
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="Content-Style-Type" content="text/css">
 <title>高田馬場周辺の列車運行情報</title>
