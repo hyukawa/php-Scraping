e0e365589b26bfc702d799e7626f007a505f02f4
hyukawa <hyukawa@live.jp>
2018-04-01 15:29:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ print<<<eof
    <tr height=30px><td bgcolor="#006600"><img src=/unkoujouhou-240px-46px-ffffff-F105.png width="20%"></td></tr>
   </table>
   <table>
-   <tr height=50px><td bgcolor="#80c241" width="10%"></td><td width="45%"><img src=/yamanote-226px-73px-000000-F105.png width="30%"></td><td class="entry-content" width="45%">$led_pat[0]</td></tr>
+   <tr height=50px><td bgcolor="#80c241" width="10%"></td><td width="45%"><img src=/yamanote-226px-51px-000000-F105.png width="30%"></td><td class="entry-content" width="45%">$led_pat[0]</td></tr>
    <tr height=50px><td bgcolor="#009bbf" width="10%"></td><td width="45%"><img src=/touzaisen-226px-73px-000000-F105.png width="80%"></td><td class="entry-content" width="45%">$led_pat[1]</td></tr>
    <tr height=50px><td bgcolor="#9c5e31" width="10%"></td><td width="45%"><img src=/fukutoshinsen-226px-73px-000000-F105.png width="90%"></td><td class="entry-content" width="45%">$led_pat[2]</td></tr>
   </table> 
