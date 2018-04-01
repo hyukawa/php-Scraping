7fe5e0f9f9f7964ba7466ef8fbc47aa2d7343536
hyukawa <hyukawa@live.jp>
2018-04-01 15:42:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -149,9 +149,9 @@ print<<<eof
    <tr height=30px><td bgcolor="#006600"><img src=/unkoujouhou-240px-46px-ffffff-F105.png width="50%"></td></tr>
   </table>
   <table>
-   <tr height=50px><td bgcolor="#80c241" width="10%"></td><td width="60%"><img src=/yamanote-226px-51px-000000-F105.png width="100%"></td><td class="entry-content" width="30%">$led_pat[0]</td></tr>
-   <tr height=50px><td bgcolor="#009bbf" width="10%"></td><td width="60%"><img src=/touzaisen-226px-73px-000000-F105.png width="100%"></td><td class="entry-content" width="30%">$led_pat[1]</td></tr>
-   <tr height=50px><td bgcolor="#9c5e31" width="10%"></td><td width="60%"><img src=/fukutoshinsen-226px-73px-000000-F105.png width="100%"></td><td class="entry-content" width="30%">$led_pat[2]</td></tr>
+   <tr height=50px><td bgcolor="#80c241" width="10%"></td><td width="70%"><img src=/yamanote-226px-51px-000000-F105.png width="100%"></td><td class="entry-content" width="20%">$led_pat[0]</td></tr>
+   <tr height=50px><td bgcolor="#009bbf" width="10%"></td><td width="70%"><img src=/touzaisen-226px-73px-000000-F105.png width="100%"></td><td class="entry-content" width="20%">$led_pat[1]</td></tr>
+   <tr height=50px><td bgcolor="#9c5e31" width="10%"></td><td width="70%"><img src=/fukutoshinsen-226px-73px-000000-F105.png width="100%"></td><td class="entry-content" width="20%">$led_pat[2]</td></tr>
   </table> 
 eof;
    //<tr><td bgcolor="#00ff00"><font size="7" color="#ffffff">運行情報</font></td>
