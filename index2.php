f9eb1e62ea3f2ea14b87381df82517ab2d5c1207
hyukawa <hyukawa@live.jp>
2018-04-01 15:08:39 +0900

    Update index2.php

--- index2.php
+++ index2.php
@@ -30,8 +30,8 @@ body	{
     height:auto;
 /* 画像を上下左右に中央配置する（絶対指定） */
   position: absolute;
-  top: 50%;
-  left: 5%;
+  top:50%;
+  left:50%;
   -webkit-transform: translate(-50%, -50%);
   -ms-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
